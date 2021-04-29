7f8efde5a486bf74852eca7a26c62842458e7502
EtWnn <eti1ww@gmail.com>
2021-04-29 20:00:52 +0200

    add transaction tables

--- ScanWatch/storage/tables.py
+++ ScanWatch/storage/tables.py
@@ -39,3 +39,141 @@ class Table:
 
         if self.primary_key is not None:
             setattr(self, self.primary_key, self.primary_key)
+
+
+def get_normal_transaction_table(address: str, scan_type: str):
+    """
+    Return the table used to store the normal transactions for an address and a scan type
+
+    :param address: address of the transactions
+    :type address: str
+    :param scan_type: scan type: ether or bsc
+    :type scan_type: str
+    :return: table to store the transactions
+    :rtype: Table
+    """
+    rows = [
+        'blockNumber',
+        'timeStamp',
+        'hash',
+        'nonce',
+        'blockHash',
+        'transactionIndex',
+        'from',
+        'to',
+        'value',
+        'gas',
+        'gasPrice',
+        'isError',
+        'txreceipt_status',
+        'input',
+        'contractAddress',
+        'cumulativeGasUsed',
+        'gasUsed',
+        'confirmations',
+    ]
+    row_types = len(rows) * ['TEXT']
+    return Table(f"{scan_type}_{address}_normal_transaction", rows, row_types)
+
+
+def get_erc20_transaction_table(address: str, scan_type: str):
+    """
+    Return the table used to store the erc20 transactions for an address and a scan type
+
+    :param address: address of the transactions
+    :type address: str
+    :param scan_type: scan type: ether or bsc
+    :type scan_type: str
+    :return: table to store the transactions
+    :rtype: Table
+    """
+    rows = [
+        'blockNumber',
+        'timeStamp',
+        'hash',
+        'nonce',
+        'blockHash',
+        'from',
+        'contractAddress',
+        'to',
+        'value',
+        'tokenName',
+        'tokenSymbol',
+        'tokenDecimal',
+        'transactionIndex',
+        'gas',
+        'gasPrice',
+        'gasUsed',
+        'cumulativeGasUsed',
+        'input',
+        'confirmations',
+    ]
+    row_types = len(rows) * ['TEXT']
+    return Table(f"{scan_type}_{address}_erc20_transaction", rows, row_types)
+
+
+def get_erc721_transaction_table(address: str, scan_type: str):
+    """
+    Return the table used to store the erc721 transactions for an address and a scan type
+
+    :param address: address of the transactions
+    :type address: str
+    :param scan_type: scan type: ether or bsc
+    :type scan_type: str
+    :return: table to store the transactions
+    :rtype: Table
+    """
+    rows = [
+        'blockNumber',
+        'timeStamp',
+        'hash',
+        'nonce',
+        'blockHash',
+        'from',
+        'contractAddress',
+        'to',
+        'tokenID',
+        'tokenName',
+        'tokenSymbol',
+        'tokenDecimal',
+        'transactionIndex',
+        'gas',
+        'gasPrice',
+        'gasUsed',
+        'cumulativeGasUsed',
+        'input',
+        'confirmations',
+    ]
+    row_types = len(rows) * ['TEXT']
+    return Table(f"{scan_type}_{address}_erc721_transaction", rows, row_types)
+
+
+def get_internal_transaction_table(address: str, scan_type: str):
+    """
+    Return the table used to store the internal transactions for an address and a scan type
+
+    :param address: address of the transactions
+    :type address: str
+    :param scan_type: scan type: ether or bsc
+    :type scan_type: str
+    :return: table to store the transactions
+    :rtype: Table
+    """
+    rows = [
+        'blockNumber',
+        'timeStamp',
+        'hash',
+        'from',
+        'to',
+        'value',
+        'contractAddress',
+        'input',
+        'type',
+        'gas',
+        'gasUsed',
+        'traceId',
+        'isError',
+        'errCode'
+    ]
+    row_types = len(rows) * ['TEXT']
+    return Table(f"{scan_type}_{address}_internal_transaction", rows, row_types)
