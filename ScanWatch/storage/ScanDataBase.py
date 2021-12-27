51d38b723e7f7b7ee0e56463c1856a8f6947baf4
EtWnn0x <EtWnn0x@gmail.com>
2021-12-27 12:36:30 +0100

    add net differentiation in the database model

--- ScanWatch/storage/ScanDataBase.py
+++ ScanWatch/storage/ScanDataBase.py
@@ -19,7 +19,7 @@ class ScanDataBase(DataBase):
         """
         super().__init__(name)
 
-    def add_transactions(self, address: str, nt_type: NETWORK, tr_type: TRANSACTION, transactions: List[Dict]):
+    def add_transactions(self, address: str, nt_type: NETWORK, net: str, tr_type: TRANSACTION, transactions: List[Dict]):
         """
         Add a list of transactions to the database
 
@@ -27,6 +27,8 @@ class ScanDataBase(DataBase):
         :type address: str
         :param nt_type: type of network
         :type nt_type: NETWORK
+        :param net: name of the network, used to differentiate main and test nets
+        :type net: str
         :param tr_type: type of the transaction to record
         :type tr_type: TRANSACTION
         :param transactions: list of the transaction to record
@@ -34,13 +36,13 @@ class ScanDataBase(DataBase):
         :return: None
         :rtype: None
         """
-        table = get_transaction_table(address, nt_type, tr_type)
+        table = get_transaction_table(address, nt_type, net, tr_type)
         for transaction in transactions:
             row = table.dict_to_tuple(transaction)
             self.add_row(table, row, auto_commit=False)
         self.commit()
 
-    def get_transactions(self, address: str, nt_type: NETWORK, tr_type: TRANSACTION) -> List[Dict]:
+    def get_transactions(self, address: str, nt_type: NETWORK, net: str, tr_type: TRANSACTION) -> List[Dict]:
         """
         Return the List of the transactions recorded in the database
 
@@ -48,16 +50,18 @@ class ScanDataBase(DataBase):
         :type address: str
         :param nt_type: type of network
         :type nt_type: NETWORK
+        :param net: name of the network, used to differentiate main and test nets
+        :type net: str
         :param tr_type: type of the transaction to fetch
         :type tr_type: TRANSACTION
         :return: list of the transaction recorded
         :rtype: List[Dict]
         """
-        table = get_transaction_table(address, nt_type, tr_type)
+        table = get_transaction_table(address, nt_type, net, tr_type)
         rows = self.get_all_rows(table)
         return [table.tuple_to_dict(row) for row in rows]
 
-    def get_last_block_number(self, address: str, nt_type: NETWORK, tr_type: TRANSACTION) -> int:
+    def get_last_block_number(self, address: str, nt_type: NETWORK, net: str, tr_type: TRANSACTION) -> int:
         """
         Return the last block number seen in recorded transactions (per address, type of transaction and network)
         If None are found, return 0
@@ -66,12 +70,14 @@ class ScanDataBase(DataBase):
         :type address: str
         :param nt_type: type of network
         :type nt_type: NETWORK
+        :param net: name of the network, used to differentiate main and test nets
+        :type net: str
         :param tr_type: type of the transaction to fetch
         :type tr_type: TRANSACTION
         :return: last block number
         :rtype: int
         """
-        table = get_transaction_table(address, nt_type, tr_type)
+        table = get_transaction_table(address, nt_type, net, tr_type)
         selection = f"MAX({table.blockNumber})"
         result = self.get_conditions_rows(table, selection=selection)
         default = 0
